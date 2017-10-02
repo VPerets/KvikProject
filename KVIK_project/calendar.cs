3f47ab695a1192f7804840f1cec5eb0902007451
Виталия <80501903813vs@gmail.com>
2017-10-02 11:41:19 +0300

    add mysql2

---
+++ KVIK_project/calendar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KVIK_project
+{
+    public partial class calendar : Form
+    {
+        public DateTime dateVal = DateTime.Now;
+
+        public calendar()
+        {
+            InitializeComponent();
+            this.Text = "Дата отгрузки";
+            this.MaximumSize = new Size(250,200);
+            this.MinimumSize = new Size(250, 200);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dateVal = this.dateTimePicker1.Value.Date;
+            this.Close();
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data.Linq;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string connStr = "server=mysql.zzz.com.ua" +
+               ";user=kvik" +
+               ";database=kvik2006" +
+                ";port=3306"+
+               ";password=Western2233";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+          
+            //Console.WriteLine(conn.State);
+
+
+        }
+    }
+}
